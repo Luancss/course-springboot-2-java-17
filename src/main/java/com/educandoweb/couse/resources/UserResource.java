c9b678095c3762ab831e2a97deb953a9f36f84ab
Luan Carlos <ventobo123@gmail.com>
2022-07-12 16:51:43 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/couse/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.educandoweb.couse.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoweb.couse.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
