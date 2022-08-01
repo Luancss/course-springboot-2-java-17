b2386a61f37edba3deafb2d4646f790bcf6623b6
Luan Carlos <ventobo123@gmail.com>
2022-08-01 10:51:04 -0300

    User insert

--- src/main/java/com/educandoweb/couse/resources/UserResource.java
+++ src/main/java/com/educandoweb/couse/resources/UserResource.java
@@ -1,13 +1,18 @@
 package com.educandoweb.couse.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.couse.entities.User;
 import com.educandoweb.couse.services.UserService;
@@ -31,4 +36,10 @@ public class UserResource {
 			return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
 }
